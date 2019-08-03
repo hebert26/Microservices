a2246d98ecab47668ea57003831b9ceb92c45abf
DESKTOP-OD49FTC\Hebert <heberth26@gmail.com>
2019-08-03 17:57:01 +0100

    Added api for orders and clients
    demo project

---
+++ ClientsApi/Controllers/ClientsController.cs
@@ -0,0 +1,19 @@
+﻿ 
+using System.Collections.Generic; 
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientsApi.Controllers
+{
+    [Route("api/Client")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Client 1", "client 2" };
+        } 
+     
+    }
+}
---
+++ OrdersApi/Controllers/OrdersController.cs
@@ -0,0 +1,18 @@
+﻿ 
+using System.Collections.Generic; 
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrdersApi.Controllers
+{
+    [Route("api/Orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "order 1", "order 2" };
+        } 
+    }
+}
